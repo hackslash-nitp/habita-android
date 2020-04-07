fb46436c3fc3f256ce9ff0c54fc8e4ef45bc96ca
prince-09 <princesinha7072@gmail.com>
2020-04-07 11:10:27 +0530

    OnClick given in MainActivity

--- app/src/main/java/club/hackslash/habita/MainActivity.java
+++ app/src/main/java/club/hackslash/habita/MainActivity.java
@@ -1,17 +1,28 @@
 package club.hackslash.habita;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    RelativeLayout food;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        food=findViewById(R.id.food);
+        food.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),foodActivity.class));
+            }
+        });
 
     }
 }
